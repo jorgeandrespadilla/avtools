bb03ef78d47780896e39f453adb2eb53be09f57c
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2024-06-20 11:04:43 -0500

    feat: improve video_to_audio_converter.py script validations

--- audio_transcriber/converter.py
+++ audio_transcriber/converter.py
@@ -1,7 +1,38 @@
 import subprocess
 
+from audio_transcriber.utils import file_exists
+
+SUPPORTED_INPUT_EXTENSIONS = [".mp4", ".mkv", ".avi", ".mov"]
+SUPPORTED_OUTPUT_EXTENSIONS = [".mp3", ".wav"]
+
+def _is_valid_extension(file: str, extensions: list[str]) -> bool:
+    return any(file.endswith(ext) for ext in extensions)
+
+def format_extensions(extensions: list[str]) -> str:
+    return ", ".join(extensions)
+
+
 def convert_video_to_audio(input_file: str, output_file: str, verbose: bool = False) -> None:
     """Convert video to audio."""
+
+    if not file_exists(input_file):
+        raise FileNotFoundError(f"The file '{input_file}' does not exist.")
+
+    if not _is_valid_extension(input_file, SUPPORTED_INPUT_EXTENSIONS):
+        raise ValueError(
+            f"Invalid input file extension. Supported extensions are: {format_extensions(SUPPORTED_INPUT_EXTENSIONS)}"
+        )
+
+    if not _is_valid_extension(output_file, SUPPORTED_OUTPUT_EXTENSIONS):
+        raise ValueError(
+            f"Invalid output file extension. Supported extensions are: {format_extensions(SUPPORTED_OUTPUT_EXTENSIONS)}"
+        )
+    
+    # Check if ffmpeg is installed
+    try:
+        subprocess.run(["ffmpeg", "-version"], capture_output=True)
+    except FileNotFoundError:
+        raise FileNotFoundError("ffmpeg is not installed. Please install ffmpeg before running this script.")
 
     # -vn: no video
     # -ac: audio channels
@@ -12,7 +43,8 @@ def convert_video_to_audio(input_file: str, output_file: str, verbose: bool = Fa
         "-ar", "44100",  # Audio rate
         "-ab", "128k",  # Audio bitrate
         "-ac", "1",
-        "-y", # Overwrite output file without asking for confirmation (if it exists)
+        # Overwrite output file without asking for confirmation (if it exists)
+        "-y",
         output_file
     ], capture_output=True)
 
@@ -20,4 +52,4 @@ def convert_video_to_audio(input_file: str, output_file: str, verbose: bool = Fa
         print(output.stdout.decode("utf-8") or output.stderr.decode("utf-8"))
 
     if output.returncode != 0:
-        raise Exception("Error converting video to audio")
+        raise Exception("Error converting video to audio. Enable verbose mode for more information.")
--- video_to_audio_converter.py
+++ video_to_audio_converter.py
@@ -5,7 +5,7 @@ from audio_transcriber.converter import convert_video_to_audio
 def main():
     parser = argparse.ArgumentParser(description="Convert video to audio.")
     parser.add_argument("input_file", help="Input video file path")
-    parser.add_argument("-o", "--output_file", default="output.mp3", help="Output audio file path")
+    parser.add_argument("-o", "--output_file", default="output.mp3", help="Output audio file path. If output file exists, it will be overwritten.")
     parser.add_argument("--verbose", action="store_true", help="Print ffmpeg output")
 
     args = parser.parse_args()
