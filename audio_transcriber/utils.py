73e16f0e2e078ded21d5a216ec9018d4032aca4b
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2024-06-20 19:38:18 -0500

    feat: add FilePath utility class

--- audio_transcriber/utils.py
+++ audio_transcriber/utils.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import os
 import dotenv
 
@@ -13,9 +14,94 @@ def get_env(key: str, default: str | None = None) -> str | None:
     return default
 
 
+def is_url(url: str) -> bool:
+    return url.startswith("http://") or url.startswith("https://")
+
+
 def file_exists(file_path: str) -> bool:
     return os.path.exists(file_path) and os.path.isfile(file_path)
 
 
-def is_url(url: str) -> bool:
-    return url.startswith("http://") or url.startswith("https://")
+class FilePath:
+    """
+    Class to easily handle file paths (path elements, validation and manipulation).
+
+    Notes:
+        - This class is a wrapper around the pathlib.Path class.
+        - The provided path is resolved to an absolute path.
+        - For directory paths or other path manipulations, use the pathlib.Path class directly 
+        (eg. Path('path/to/dir') / 'file.txt').
+    """
+
+    __full_path: Path
+
+    def __init__(self, path: Path | str):
+        self.__full_path = Path(path).resolve()
+
+# region Properties
+
+    @property
+    def full_path(self) -> Path:
+        """Get the full path of the file."""
+        return self.__full_path
+
+    @property
+    def directory_path(self) -> Path:
+        """Get the directory path where the file is located."""
+        return self.__full_path.parent
+
+    @property
+    def full_name(self) -> str:
+        """Get the full name of the file (with the extension)."""
+        return self.__full_path.name
+
+    @property
+    def base_name(self) -> str:
+        """Get the base name of the file (without the extension)."""
+        return self.__full_path.stem
+
+    @property
+    def extension(self) -> str:
+        """Get the file extension including the dot."""
+        return self.__full_path.suffix
+
+    @property
+    def extension_without_dot(self) -> str:
+        """Get the file extension without the dot."""
+        return self.__full_path.suffix[1:]
+
+# endregion
+
+
+# region Validation Methods
+
+    def file_exists(self) -> bool:
+        """Check if the file exists."""
+        return self.__full_path.exists() and self.__full_path.is_file()
+
+    def directory_exists(self) -> bool:
+        """Check if the directory where the file should be located exists."""
+        directory_path = Path(self.directory_path)
+        return directory_path.exists() and directory_path.is_dir()
+
+# endregion
+
+
+# region Path Manipulation Methods
+
+    def with_full_name(self, name: str) -> 'FilePath':
+        """Return a new FilePath with the provided full name (including the extension)."""
+        return FilePath(self.__full_path.with_name(name))
+
+    def with_base_name(self, name: str) -> 'FilePath':
+        """Return a new FilePath with the provided base name (without the extension)."""
+        return FilePath(self.__full_path.with_stem(name))
+
+    def with_extension(self, extension: str) -> str:
+        """Return a new FilePath with the provided extension."""
+        return str(self.__full_path.with_suffix(extension))
+
+# endregion
+
+    def __str__(self) -> str:
+        return str(self.full_path)
---
+++ tests/utils/_test_file_path.py
@@ -0,0 +1,32 @@
+from audio_transcriber.utils import FilePath
+
+
+try:
+    file_path = FilePath('test.py')
+
+    # Properties
+    print("Full Path:", file_path.full_path)
+    print("Directory Path:", file_path.directory_path)
+    print("Full Name:", file_path.full_name)
+    print("Base Name:", file_path.base_name)
+    print("File Extension:", file_path.extension)
+    print("File Extension Without Dot:", file_path.extension_without_dot)
+
+    # Validation Methods
+    print("File Exists:", file_path.file_exists())
+    print("Directory Exists:", file_path.directory_exists())
+
+    # Path Manipulation Methods
+    print("With Full Name:", file_path.with_full_name('test.txt'))
+    print("With Base Name:", file_path.with_base_name('test'))
+    print("With Extension:", file_path.with_extension('.txt'))
+
+    # Other
+    print("New File with Same Directory:", FilePath(
+        file_path.directory_path / 'new_file.py'))
+    print("New File with Nested Directory:", FilePath(
+        file_path.directory_path / 'nested' / 'new_file.py'))
+    print("Sample File with Nested Directory:", FilePath(
+        file_path.directory_path / 'nested' / file_path.full_name))
+except ValueError as e:
+    print(f"Validation error: {e}")
