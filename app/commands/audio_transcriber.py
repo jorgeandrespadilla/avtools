d56358fd392db70086c3a5b6b856193dbaa6315d
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2024-06-29 15:06:52 -0500

    refactor: update audio transcriber script to use command pattern

--- app/commands/audio_transcriber.py
+++ app/commands/audio_transcriber.py
@@ -1,8 +1,93 @@
 import json
+from typing import Self
+
+from pydantic import BaseModel, ConfigDict, computed_field, model_validator
 from app.pipelines import transcription, diarization
+from app.utils import FilePath, is_supported_extension, is_url, list_extensions
 
 SUPPORTED_INPUT_EXTENSIONS = [".mp3", ".wav"]
 SUPPORTED_OUTPUT_EXTENSIONS = [".json", ".txt"]
+
+
+class CommandParams(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    input_file: str
+    """
+    Input video file path or URL. 
+    If input file is not a URL, do not use this field directly, use the input_file_path property instead.
+    """
+
+    output_file: str
+    """Output audio file path (do not use this field directly, use the output_file_path property instead)."""
+
+    language: str | None = None
+    """Language code to use for transcription."""
+
+    hf_token: str | None = None
+    """Hugging Face API token for diarization. If omitted, diarization will be disabled."""
+
+    # Additional parameters
+    device_id: str = "cuda:0"
+    """
+    Device ID to use for processing. Use 'cuda:{#}' for GPU devices or 'mps' for Mac devices.
+    """
+
+    enable_timestamps: bool = True
+    """Enable timestamps in the transcription output."""
+
+    @computed_field
+    @property
+    def input_file_or_url(self) -> str:
+        """Get the input file path or URL as a string."""
+        if is_url(self.input_file):
+            return self.input_file
+        return str(self.input_file_path)
+
+    @computed_field
+    @property
+    def input_file_path(self) -> FilePath:
+        """Get the input file path (fails if input is a URL)."""
+        if is_url(self.input_file):
+            raise ValueError("Cannot get file path for URL input (this should not happen).")
+        return FilePath(self.input_file)
+
+    @computed_field
+    @property
+    def output_file_path(self) -> FilePath:
+        return FilePath(self.output_file)
+
+    @model_validator(mode="after")
+    def _validate_files(self) -> Self:
+        self._validate_input_file()
+
+        self._validate_output_file()
+
+        if self.hf_token and not self.enable_timestamps:
+            raise ValueError("Diarization requires timestamps to be enabled.")
+        return self
+
+    def _validate_input_file(self):
+        if is_url(self.input_file):
+            return self
+
+        if not self.input_file_path.file_exists():
+            raise FileNotFoundError(f"File not found: '{self.input_file_path.full_path}'")
+        if not is_supported_extension(self.input_file_path.extension, SUPPORTED_INPUT_EXTENSIONS):
+            raise ValueError(
+                f"Invalid input file format: '{self.input_file_path.extension_without_dot.upper()}'. Supported formats: {list_extensions(SUPPORTED_INPUT_EXTENSIONS)}"
+            )
+        return self
+
+    def _validate_output_file(self):
+        if not self.output_file_path.directory_exists():
+            raise FileNotFoundError(
+                f"Directory not found: '{self.output_file_path.directory_path}'"
+            )
+        if not is_supported_extension(self.output_file_path.extension, SUPPORTED_OUTPUT_EXTENSIONS):
+            raise ValueError(
+                f"Invalid output file format: '{self.output_file_path.extension_without_dot.upper()}'. Supported formats: {list_extensions(SUPPORTED_OUTPUT_EXTENSIONS)}"
+            )
 
 
 def build_result(transcript, outputs):
@@ -27,6 +112,7 @@ def group_chunks_by_speaker(chunks: list[dict]) -> list[dict]:
     return new_chunks
 
 
+# TODO: Deprecate this format and move it to the transcript formatter script
 def transcript_to_text(transcript: dict, group_by_speaker: bool = False) -> str:
     if "speakers" in transcript:
         if group_by_speaker:
@@ -40,41 +126,33 @@ def transcript_to_text(transcript: dict, group_by_speaker: bool = False) -> str:
         return transcript["text"]
 
 
-def execute(
-    input_file: str,
-    output_file: str,
-    language: str | None = None,
-    hf_token: str | None = None,
-    device_id: str = "cuda:0",  # 'cuda:{#}' or 'mps' for Mac devices
-    enable_timestamps: bool = True,
-):
-    if hf_token and not enable_timestamps:
-        raise ValueError("Diarization requires timestamps to be enabled.")
-
+def execute(params: CommandParams) -> None:
     # Transcription
     transcription_params = transcription.PipelineParams(
-        input_file=input_file,
-        device_id=device_id,
-        enable_timestamps=enable_timestamps,
-        language=language,
+        input_file=params.input_file_or_url,
+        device_id=params.device_id,
+        enable_timestamps=params.enable_timestamps,
+        language=params.language,
     )
     transcription_result = transcription.run(transcription_params)
 
     # Diarization
-    if hf_token:
+    if params.hf_token:
         diarization_params = diarization.PipelineParams(
-            input_file=input_file,
-            device_id=device_id,
-            hf_token=hf_token,
+            input_file=params.input_file_or_url,
+            device_id=params.device_id,
+            hf_token=params.hf_token,
         )
-        diarization_result = diarization.run(diarization_params, transcription_result) # Speakers transcript
+        diarization_result = diarization.run(
+            diarization_params, transcription_result
+        )  # Speakers transcript
         result = build_result(diarization_result, transcription_result)
     else:
         result = build_result([], transcription_result)
 
-    if output_file.endswith(".txt"):
-        with open(output_file, "w", encoding="utf8") as fp:
+    if params.output_file_path.extension == ".txt":
+        with open(params.output_file_path.full_path, "w", encoding="utf8") as fp:
             fp.write(transcript_to_text(result, group_by_speaker=True))
     else:
-        with open(output_file, "w", encoding="utf8") as fp:
+        with open(params.output_file_path.full_path, "w", encoding="utf8") as fp:
             json.dump(result, fp, ensure_ascii=False)
--- audio_transcriber.py
+++ audio_transcriber.py
@@ -1,22 +1,20 @@
 import argparse
-from pydantic import BaseModel
 from rich import print as rprint
 
 from app.utils import (
     ArgumentHelpFormatter,
-    FilePath,
     get_env,
     handle_errors,
-    is_supported_extension,
-    is_url,
-    list_extensions,
 )
 from app.commands import audio_transcriber
 
 
+HUGGING_FACE_TOKEN_ENV_VAR = "HUGGING_FACE_TOKEN"
+
+
 def _parse_args():
     parser = argparse.ArgumentParser(
-        description="Audio Transcriber", formatter_class=ArgumentHelpFormatter
+        description="Transcribe audio files.", formatter_class=ArgumentHelpFormatter
     )
     parser.add_argument(
         "-i",
@@ -45,64 +43,23 @@ def _parse_args():
         required=False,
         default=None,
         type=str,
-        help="Provide a hf.co/settings/token for Pyannote.audio to diarise the audio clips. If not provided, it will be searched in the environment variables. If not found, diarization will be skipped. To use this feature, follow the instructions in https://huggingface.co/pyannote/speaker-diarization-3.1.",
+        help=f"Provide a hf.co/settings/token for Pyannote.audio to diarise the audio clips. If not provided, it will be searched in the environment variables ({HUGGING_FACE_TOKEN_ENV_VAR}). If not found, diarization will be skipped. To use this feature, follow the instructions in https://huggingface.co/pyannote/speaker-diarization-3.1.",
     )
     return parser.parse_args()
-
-
-class CLIArgs(BaseModel):
-    input: str
-    output: str
-    language: str | None = None
-    hf_token: str | None = None
-
-
-def validated_input_file(input_file) -> str:
-    if is_url(input_file):
-        return input_file
-
-    input_path = FilePath(input_file)
-    if not input_path.file_exists():
-        raise FileNotFoundError(f"File not found: '{input_path.full_path}'")
-    if not is_supported_extension(
-        input_path.extension, audio_transcriber.SUPPORTED_INPUT_EXTENSIONS
-    ):
-        raise ValueError(
-            f"Invalid input file format: '{input_path.extension_without_dot.upper()}'. Supported formats: {list_extensions(audio_transcriber.SUPPORTED_INPUT_EXTENSIONS)}"
-        )
-
-    return str(input_path.full_path)
-
-
-def validated_output_path(output_path) -> str:
-    output_path = FilePath(output_path)
-    if not output_path.directory_exists():
-        raise FileNotFoundError(f"Directory not found: '{output_path.directory_path}'")
-    if not is_supported_extension(
-        output_path.extension, audio_transcriber.SUPPORTED_OUTPUT_EXTENSIONS
-    ):
-        raise ValueError(
-            f"Invalid output file format: '{output_path.extension_without_dot.upper()}'. Supported formats: {list_extensions(audio_transcriber.SUPPORTED_OUTPUT_EXTENSIONS)}"
-        )
-
-    return str(output_path.full_path)
 
 
 @handle_errors
 def main():
     args = _parse_args()
-    args = CLIArgs(**args.__dict__)
-    hf_token = args.hf_token or get_env("HUGGING_FACE_TOKEN")
+    hf_token = args.hf_token or get_env(HUGGING_FACE_TOKEN_ENV_VAR)
 
-    input_file = validated_input_file(args.input)
-    output_path = validated_output_path(args.output)
-
-    audio_transcriber.execute(
-        input_file,
-        output_path,
+    command_params = audio_transcriber.CommandParams(
+        input_file=args.input,
+        output_file=args.output,
         language=args.language,
         hf_token=hf_token,  # Use diarization model
     )
+    audio_transcriber.execute(command_params)
 
     rprint(f"[bold green]Transcription saved to '{args.output}'[/bold green]")
 
